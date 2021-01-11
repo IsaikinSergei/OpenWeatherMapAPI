3d0f6a4c2343d01eb56729be21118437417b20e8
IsaikinSergei <akinfest007@gmail.com>
2021-01-11 23:21:25 +0400

    Initial Commit

---
+++ OpenWeatherMapAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenWeatherMapAPI
+//
+//  Created by Sergei Isaikin on 11.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
